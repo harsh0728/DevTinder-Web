f12d4867a274cef282a3e2c2905dfab3bb991179
Harsh Kumar <hk.develop07@gmail.com>
2026-01-15 00:24:33 +0530

    FIX: changed socket url

--- src/config/socket.js
+++ src/config/socket.js
@@ -1,7 +1,7 @@
 import {io} from "socket.io-client";
 import { API_BASE } from "../api/apiFetch";
 
-export const socketConnection=io("http://localhost:3000",
+export const socketConnection=io("https://devtinder-1-pqv8.onrender.com",
     {
         withCredentials:true,
     }
