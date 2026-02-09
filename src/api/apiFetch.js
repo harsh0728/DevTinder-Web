3936a54b159ad5b67cda86991662b29678a81e5d
Harsh Kumar <hk.develop07@gmail.com>
2026-02-09 16:45:10 +0530

    chore: switch to development mode for local testing

--- src/api/apiFetch.js
+++ src/api/apiFetch.js
@@ -1,5 +1,6 @@
 //export const API_BASE = 'http://localhost:3000/api'; // development
-export const API_BASE = 'https://devtinder-1-pqv8.onrender.com/api'; // production
+//export const API_BASE = 'https://devtinder-1-pqv8.onrender.com/api'; // production
+export const API_BASE=import.meta.env.VITE_CLIENT_ENV==="production"?'https://devtinder-1-pqv8.onrender.com/api':'http://localhost:3000/api';
 
 export const apiFetch = async (endpoint, options = {}) => {
   const response = await fetch(`${API_BASE}${endpoint}`, {  
--- src/config/socket.js
+++ src/config/socket.js
@@ -1,7 +1,7 @@
 import {io} from "socket.io-client";
 import { API_BASE } from "../api/apiFetch";
 
-export const socketConnection=io("https://devtinder-1-pqv8.onrender.com",
+export const socketConnection=io(import.meta.VITE_CLIENT_ENV==="production"?"https://devtinder-1-pqv8.onrender.com":'http://localhost:3000',
     {
         withCredentials:true,
     }
