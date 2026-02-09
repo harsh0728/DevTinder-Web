2ecb50414448592a9119270ca03c5d71deaf2b78
Harsh Kumar <hk.develop07@gmail.com>
2026-02-09 21:27:21 +0530

    Feat: Added Dynamic Link for production and development

--- src/components/GoogleLoginButton.jsx
+++ src/components/GoogleLoginButton.jsx
@@ -1,14 +1,15 @@
 export default function GoogleLoginButton() {
   const handleLogin = () => {
-    window.location.href = "http://localhost:3000/api/auth/google";
+    window.location.href =
+  import.meta.env.VITE_CLIENT_ENV === "production"?"https://devtinder-1-pqv8.onrender.com/api/auth/google": "http://localhost:3000/api/auth/google";
   };
 
   return (
     <button
       onClick={handleLogin}
-      className="w-full py-3 rounded-lg bg-white text-black font-semibold shadow flex items-center justify-center gap-2"
+      className="w-full mt-3 py-2 rounded-lg bg-white text-black font-semibold shadow flex items-center justify-center gap-2"
     >
-      <img src="/google.svg" className="w-5 h-5" />
+      <img src={"../public/google.jpg"} className="w-10 h-10" />
       Continue with Google
     </button>
   );
